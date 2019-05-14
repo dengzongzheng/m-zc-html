f0815beda6889fcd97411446071a3f78c3befaf4
zori0 <776021342@qq.com>
2019-05-14 21:20:21 +0800

    init

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -5,9 +5,20 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, '../src/index.js'),
+    performance: {
+        // false | "error" | "warning" // 不显示性能提示 | 以错误形式提示 | 以警告...
+        hints: "warning",
+        // 开发环境设置较大防止警告
+        // 根据入口起点的最大体积，控制webpack何时生成性能提示,整数类型,以字节为单位
+        maxEntrypointSize: 5000000,
+        // 最大单个资源体积，默认250000 (bytes)
+        maxAssetSize: 3000000
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../dist'),
+        //按需加载
+        chunkFilename:'[name]_[chunkhash:8].js'
     },
     resolve: {
         extensions: ['*', '.js', '.jsx'],
--- src/routers/website-router.js
+++ src/routers/website-router.js
@@ -1,58 +1,62 @@
-import WebsiteIndex from '@pages/website/index/index';
-import WebsiteDetail from '@pages/website/detail/detail';
-import List from '@pages/website/list/list';
-import About from '@pages/website/about/about';
-import Us from '@pages/website/us/us';
+import 'bundle-loader'
+import Bundle from '@/components/lazyload/Bundle.js';
+import lazyLoad from '@/components/lazyload/LazyLoad';
+import WebsiteIndex from 'bundle-loader?lazy&name=websiteIndex!@pages/website/index/index';
+import WebsiteDetail from 'bundle-loader?lazy&name=websiteDetail!@pages/website/detail/detail';
+import List from 'bundle-loader?lazy&name=list!@pages/website/list/list';
+import About from 'bundle-loader?lazy&name=about!@pages/website/about/about';
+import Us from 'bundle-loader?lazy&name=us!@pages/website/us/us';
+
 const routers = [
     {
         path:'/',
         exact:true,
-        component:WebsiteIndex
+        component: lazyLoad(WebsiteIndex)
     },
     {
         path:'/index',
         exact:true,
-        component:WebsiteIndex
+        component: lazyLoad(WebsiteIndex)
     },
     {
         path:'/detail',
         exact:true,
-        component:WebsiteDetail
+        component: lazyLoad(WebsiteDetail)
     },
     {
         path:'/others',
         exact:true,
-        component:List
+        component: lazyLoad(List)
     },
     {
         path:'/pictures',
         exact:true,
-        component:List
+        component: lazyLoad(List)
     },
     {
         path:'/porcelains',
         exact:true,
-        component:List
+        component: lazyLoad(List)
     },
     {
         path:'/jades',
         exact:true,
-        component:List
+        component: lazyLoad(List)
     },
     {
         path:'/recommended',
         exact:true,
-        component:List
+        component: lazyLoad(List)
     },
     {
         path:'/about',
         exact:true,
-        component:About
+        component: lazyLoad(About)
     },
     {
         path:'/us',
         exact:true,
-        component:Us
+        component: lazyLoad(Us)
     }
 ];
 
